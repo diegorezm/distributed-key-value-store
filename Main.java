fb2e9d35014e0b4ac9bfbf782c2f7b53ebfd79b2
diegorezm <dmourarezende@gmail.com>
2026-07-06 20:01:59 -0300

    added logging library

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
-import server.ServerStore;
+import server.StoreNodeServer;
 
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS com.google.code.gson:gson:2.11.0
+//DEPS org.slf4j:slf4j-api:1.7.36
+//DEPS org.slf4j:slf4j-simple:1.7.36
+//JAVA_OPTS -Dorg.slf4j.simpleLogger.defaultLogLevel=debug
+//JAVA_OPTS -Dorg.slf4j.simpleLogger.showDateTime=true
+//JAVA_OPTS -Dorg.slf4j.simpleLogger.dateTimeFormat=yyyy-MM-dd HH:mm:ss
 //SOURCES **/*.java
 
 void main() {
-    ServerStore server = new ServerStore(4000);
-    server.run();
+    StoreNodeServer node = new StoreNodeServer(4000);
+    node.run();
 }
